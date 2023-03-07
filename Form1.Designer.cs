0f8587f2f09056a46c26a45dd0dcb12266e5bff8
Adam Rafif <adamrafif@student.telkomuniversity.ac.id>
2023-03-07 16:43:25 +0700

    add GUI calculator
    caldulator dapat melakukan penjumlahan angka lebih dari satu digit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.satu = new System.Windows.Forms.Button();
+            this.dua = new System.Windows.Forms.Button();
+            this.tiga = new System.Windows.Forms.Button();
+            this.tambah = new System.Windows.Forms.Button();
+            this.samaDengan = new System.Windows.Forms.Button();
+            this.nol = new System.Windows.Forms.Button();
+            this.empat = new System.Windows.Forms.Button();
+            this.lima = new System.Windows.Forms.Button();
+            this.enam = new System.Windows.Forms.Button();
+            this.tujuh = new System.Windows.Forms.Button();
+            this.delapan = new System.Windows.Forms.Button();
+            this.sembilan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // satu
+            // 
+            this.satu.Location = new System.Drawing.Point(137, 168);
+            this.satu.Name = "satu";
+            this.satu.Size = new System.Drawing.Size(60, 63);
+            this.satu.TabIndex = 1;
+            this.satu.Text = "1";
+            this.satu.UseVisualStyleBackColor = true;
+            this.satu.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dua
+            // 
+            this.dua.Location = new System.Drawing.Point(203, 168);
+            this.dua.Name = "dua";
+            this.dua.Size = new System.Drawing.Size(61, 63);
+            this.dua.TabIndex = 2;
+            this.dua.Text = "2";
+            this.dua.UseVisualStyleBackColor = true;
+            this.dua.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tiga
+            // 
+            this.tiga.Location = new System.Drawing.Point(270, 168);
+            this.tiga.Name = "tiga";
+            this.tiga.Size = new System.Drawing.Size(61, 63);
+            this.tiga.TabIndex = 3;
+            this.tiga.Text = "3";
+            this.tiga.UseVisualStyleBackColor = true;
+            this.tiga.Click += new System.EventHandler(this.tiga_Click);
+            // 
+            // tambah
+            // 
+            this.tambah.Location = new System.Drawing.Point(137, 99);
+            this.tambah.Name = "tambah";
+            this.tambah.Size = new System.Drawing.Size(61, 63);
+            this.tambah.TabIndex = 4;
+            this.tambah.Text = "+";
+            this.tambah.UseVisualStyleBackColor = true;
+            this.tambah.Click += new System.EventHandler(this.tambah_Click);
+            // 
+            // samaDengan
+            // 
+            this.samaDengan.Location = new System.Drawing.Point(203, 99);
+            this.samaDengan.Name = "samaDengan";
+            this.samaDengan.Size = new System.Drawing.Size(61, 63);
+            this.samaDengan.TabIndex = 5;
+            this.samaDengan.Text = "=";
+            this.samaDengan.UseVisualStyleBackColor = true;
+            this.samaDengan.Click += new System.EventHandler(this.samaDengan_Click);
+            // 
+            // nol
+            // 
+            this.nol.Location = new System.Drawing.Point(270, 99);
+            this.nol.Name = "nol";
+            this.nol.Size = new System.Drawing.Size(61, 63);
+            this.nol.TabIndex = 6;
+            this.nol.Text = "0";
+            this.nol.UseVisualStyleBackColor = true;
+            this.nol.Click += new System.EventHandler(this.nol_Click);
+            // 
+            // empat
+            // 
+            this.empat.Location = new System.Drawing.Point(137, 237);
+            this.empat.Name = "empat";
+            this.empat.Size = new System.Drawing.Size(61, 63);
+            this.empat.TabIndex = 7;
+            this.empat.Text = "4";
+            this.empat.UseVisualStyleBackColor = true;
+            this.empat.Click += new System.EventHandler(this.empat_Click);
+            // 
+            // lima
+            // 
+            this.lima.Location = new System.Drawing.Point(203, 237);
+            this.lima.Name = "lima";
+            this.lima.Size = new System.Drawing.Size(61, 63);
+            this.lima.TabIndex = 8;
+            this.lima.Text = "5";
+            this.lima.UseVisualStyleBackColor = true;
+            this.lima.Click += new System.EventHandler(this.lima_Click);
+            // 
+            // enam
+            // 
+            this.enam.Location = new System.Drawing.Point(270, 237);
+            this.enam.Name = "enam";
+            this.enam.Size = new System.Drawing.Size(61, 63);
+            this.enam.TabIndex = 9;
+            this.enam.Text = "6";
+            this.enam.UseVisualStyleBackColor = true;
+            this.enam.Click += new System.EventHandler(this.enam_Click);
+            // 
+            // tujuh
+            // 
+            this.tujuh.Location = new System.Drawing.Point(136, 306);
+            this.tujuh.Name = "tujuh";
+            this.tujuh.Size = new System.Drawing.Size(61, 63);
+            this.tujuh.TabIndex = 10;
+            this.tujuh.Text = "7";
+            this.tujuh.UseVisualStyleBackColor = true;
+            this.tujuh.Click += new System.EventHandler(this.tujuh_Click);
+            // 
+            // delapan
+            // 
+            this.delapan.Location = new System.Drawing.Point(203, 306);
+            this.delapan.Name = "delapan";
+            this.delapan.Size = new System.Drawing.Size(61, 63);
+            this.delapan.TabIndex = 11;
+            this.delapan.Text = "8";
+            this.delapan.UseVisualStyleBackColor = true;
+            this.delapan.Click += new System.EventHandler(this.delapan_Click);
+            // 
+            // sembilan
+            // 
+            this.sembilan.Location = new System.Drawing.Point(270, 306);
+            this.sembilan.Name = "sembilan";
+            this.sembilan.Size = new System.Drawing.Size(61, 63);
+            this.sembilan.TabIndex = 12;
+            this.sembilan.Text = "9";
+            this.sembilan.UseVisualStyleBackColor = true;
+            this.sembilan.Click += new System.EventHandler(this.sembilan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.Controls.Add(this.sembilan);
+            this.Controls.Add(this.delapan);
+            this.Controls.Add(this.tujuh);
+            this.Controls.Add(this.enam);
+            this.Controls.Add(this.lima);
+            this.Controls.Add(this.empat);
+            this.Controls.Add(this.nol);
+            this.Controls.Add(this.samaDengan);
+            this.Controls.Add(this.tambah);
+            this.Controls.Add(this.tiga);
+            this.Controls.Add(this.dua);
+            this.Controls.Add(this.satu);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button satu;
+        private Button dua;
+        private Button tiga;
+        private Button tambah;
+        private Button samaDengan;
+        private Button nol;
+        private Button empat;
+        private Button lima;
+        private Button enam;
+        private Button tujuh;
+        private Button delapan;
+        private Button sembilan;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,93 @@
+using System.Reflection.Emit;
+
 namespace Modul3_1302210008
 {
     public partial class Form1 : Form
     {
+        int angka;
+        int hasil = 0;
         public Form1()
         {
             InitializeComponent();
+            label1.Text= "";
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "1";
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "2";
+        }
+
+        private void tiga_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "3";
+        }
+
+        private void empat_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "4";
+        }
+
+        private void lima_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "5";
+        }
+
+        private void enam_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "6";
+        }
+
+        private void tujuh_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "7";
+        }
+
+        private void delapan_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "8";
+        }
+
+        private void sembilan_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "9";
+        }
+
+        private void tambah_Click(object sender, EventArgs e)
+        {
+            angka = int.Parse(label1.Text);
+            hasil += angka;
+            label1.Text = "+";
+            label1.Text = "";
+        }
+
+        private void samaDengan_Click(object sender, EventArgs e)
+        {
+            angka = int.Parse(label1.Text);
+            hasil += angka;
+            label1.Text = hasil.ToString();
+        }
+
+        private void nol_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
